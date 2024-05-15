00e792a5b9d3671f35c73b21f4016ef3533a723a
Serhii_Nosko <Serhii_Nosko@epam.com>
2024-05-15 14:47:42 +0300

    EDGLTI-11. Add declaration of BouncyCastleFipsProvider in the module

--- src/main/java/org/folio/edge/ltiCourses/utils/LtiCoursesOkapiClientFactory.java
+++ src/main/java/org/folio/edge/ltiCourses/utils/LtiCoursesOkapiClientFactory.java
@@ -2,12 +2,15 @@ package org.folio.edge.ltiCourses.utils;
 
 import static org.folio.edge.core.Constants.SYS_KEYSTORE_PASSWORD;
 import static org.folio.edge.core.Constants.SYS_KEYSTORE_PATH;
+import static org.folio.edge.core.Constants.SYS_KEYSTORE_PROVIDER;
+import static org.folio.edge.core.Constants.SYS_KEYSTORE_TYPE;
 import static org.folio.edge.core.Constants.SYS_KEY_ALIAS;
+import static org.folio.edge.core.Constants.SYS_KEY_ALIAS_PASSWORD;
 import static org.folio.edge.core.Constants.SYS_OKAPI_URL;
 import static org.folio.edge.core.Constants.SYS_REQUEST_TIMEOUT_MS;
+import static org.folio.edge.core.Constants.SYS_SSL_ENABLED;
 
 import io.vertx.core.json.JsonObject;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.edge.core.utils.OkapiClientFactory;
@@ -23,12 +26,16 @@ public class LtiCoursesOkapiClientFactory {
   public static OkapiClientFactory createInstance(Vertx vertx, JsonObject config) {
     String okapiUrl = config.getString(SYS_OKAPI_URL);
     int reqTimeoutMs = config.getInteger(SYS_REQUEST_TIMEOUT_MS);
-    String keystorePath = config.getString(SYS_KEYSTORE_PATH);
-    String keystorePassword = config.getString(SYS_KEYSTORE_PASSWORD);
-    String keyAlias = config.getString(SYS_KEY_ALIAS);
-    if (StringUtils.isNotBlank(keystorePath) && StringUtils.isNotBlank(keystorePassword)) {
+    boolean isSslEnabled = config.getBoolean(SYS_SSL_ENABLED);
+    if (isSslEnabled) {
       logger.info("Creating OkapiClientFactory with Enhance HTTP Endpoint Security and TLS mode enabled");
-      return new OkapiClientFactory(vertx, okapiUrl, reqTimeoutMs, keystorePath, keystorePassword, keyAlias);
+      String keystoreType = config.getString(SYS_KEYSTORE_TYPE);
+      String keystoreProvider = config.getString(SYS_KEYSTORE_PROVIDER);
+      String keystorePath = config.getString(SYS_KEYSTORE_PATH);
+      String keystorePassword = config.getString(SYS_KEYSTORE_PASSWORD);
+      String keyAlias = config.getString(SYS_KEY_ALIAS);
+      String keyAliasPassword = config.getString(SYS_KEY_ALIAS_PASSWORD);
+      return new OkapiClientFactory(vertx, okapiUrl, reqTimeoutMs, keystoreType, keystoreProvider, keystorePath, keystorePassword, keyAlias, keyAliasPassword);
     } else {
       return new OkapiClientFactory(vertx, okapiUrl,  reqTimeoutMs);
     }
